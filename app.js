4e84215de5e9d2c959aab18d1151fecb607432d9
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2025-01-12 05:31:04 +0100

    Add in operator functions

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log(document.querySelector("h1").textContent);
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
