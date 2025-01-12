e16d8f0b5164350ad3dab4371d7bdfdc2cbb6682
OMAR SAROUR <74824708+OneSrX@users.noreply.github.com>
2025-01-12 05:32:29 +0100

    Add operation variables and the operate function

--- app.js
+++ app.js
@@ -13,3 +13,26 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let FirstNum;
+let operator;
+let SecondNum;
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+    default:
+      return "Invalid operator";
+  }
+}
